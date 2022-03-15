136f5d7ac6621d330067e3d0863a3282ce51a995
Gustavo Barbosa <gbdsantos0@inf.ufpel.edu.br>
2022-03-15 17:34:08 -0300

    feat: aula6

---
+++ modulo3/aulas/aula6/pessoaapi/src/main/java/com/dbc/pessoaapi/service/PessoaService.java
@@ -0,0 +1,62 @@
+package com.dbc.pessoaapi.service;
+
+import com.dbc.pessoaapi.dto.PessoaCreateDTO;
+import com.dbc.pessoaapi.dto.PessoaDTO;
+import com.dbc.pessoaapi.entity.Pessoa;
+import com.dbc.pessoaapi.repository.PessoaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PessoaService {
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public PessoaDTO create(PessoaCreateDTO pessoa) throws Exception {
+        Pessoa pessoaEntity = objectMapper.convertValue(pessoa, Pessoa.class);
+        Pessoa pessoaCriada = pessoaRepository.create(pessoaEntity);
+        PessoaDTO pessoaDTO = objectMapper.convertValue(pessoaCriada, PessoaDTO.class);
+        return pessoaDTO;
+    }
+
+    public List<PessoaDTO> list() throws Exception{
+        return pessoaRepository.list()
+                .stream()
+                .map(pessoa -> objectMapper.convertValue(pessoa, PessoaDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public PessoaDTO update(Integer id, PessoaCreateDTO pessoaAtualizar) throws Exception {
+        Pessoa pessoaEntity = objectMapper.convertValue(pessoaAtualizar, Pessoa.class);
+        Pessoa pessoaAtualizada = pessoaRepository.update(id, pessoaEntity);
+        PessoaDTO pessoaDTO = objectMapper.convertValue(pessoaAtualizada, PessoaDTO.class);
+        return pessoaDTO;
+    }
+
+    public PessoaDTO delete(Integer id) throws Exception {
+        Pessoa pessoaDeletada = pessoaRepository.delete(id);
+        PessoaDTO pessoaDTO = objectMapper.convertValue(pessoaDeletada, PessoaDTO.class);
+        return pessoaDTO;
+    }
+
+    public PessoaDTO getById(Integer id) throws Exception {
+        Pessoa pessoa = pessoaRepository.getById(id);
+        PessoaDTO pessoaDTO = objectMapper.convertValue(pessoa, PessoaDTO.class);
+        return pessoaDTO;
+    }
+
+    public List<PessoaDTO> listByName(String nome) throws Exception {
+        return pessoaRepository.listByName(nome)
+                .stream()
+                .map(pessoa -> objectMapper.convertValue(pessoa, PessoaDTO.class))
+                .collect(Collectors.toList());
+    }
+
+}
