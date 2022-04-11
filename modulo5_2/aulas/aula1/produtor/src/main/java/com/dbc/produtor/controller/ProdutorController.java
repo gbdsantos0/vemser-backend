a7e52b178013aaeeb8d9d661591fecdf3d8b2e75
Gustavo Barbosa <gbdsantos0@inf.ufpel.edu.br>
2022-04-11 18:10:22 -0300

    base homework 1 modulo 5

---
+++ modulo5_2/aulas/aula1/produtor/src/main/java/com/dbc/produtor/controller/ProdutorController.java
@@ -0,0 +1,11 @@
+package com.dbc.produtor.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+@RequiredArgsConstructor
+public class ProdutorController {
+}
