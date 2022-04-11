9c8857aa3136ba038d6bd7ad9baef4403287b111
Gustavo Barbosa <gbdsantos0@inf.ufpel.edu.br>
2022-04-11 16:56:53 -0300

    feat: aula 1 modulo 5 base

--- modulo4/homeworks/homework4/mongodbjava/src/main/java/Main.java
+++ modulo4/homeworks/homework4/mongodbjava/src/main/java/Main.java
@@ -23,45 +23,44 @@ public class Main {
         MongoCollection<Document> mochilas = mongoDatabase.getCollection("mochila");
 
         //todo CRIANDO NOVO DOCUMENTO E INSERINDO
-        Document novoPokemon = new Document("mochila_id",3)
-                .append("racaPokemon", "Rayquaza")
-                .append("peso",2000.00)
-                .append("sexo","FEMININO")
-                .append("level",8000)
-                .append("dificuldade","DIFICIL")
-                .append("tipos",Arrays.asList(
-                        new Document("tipo1","DRAGON"),
-                        new Document("tipo2","FLYING")
-                        )
-                )
-                .append("raridade","SUPER_RARO");
-
-        pokemons.insertOne(novoPokemon);
-
-        Document novaMochila = new Document("_id",3)
-                .append("quantidadePokeball",99)
-                .append("quantidadeGreatball",99)
-                .append("quantidadeMasterball",99)
-                .append("quantidadeHeavyball",99)
-                .append("quantidadeNetball",99);
-        Document novaMochila1 = new Document("_id",4)
-                .append("quantidadePokeball",0)
-                .append("quantidadeGreatball",0)
-                .append("quantidadeMasterball",0)
-                .append("quantidadeHeavyball",0)
-                .append("quantidadeNetball",0);
-
-        mochilas.insertMany(Arrays.asList(novaMochila,novaMochila1));
+//        Document novoPokemon = new Document("mochila_id",3)
+//                .append("racaPokemon", "Rayquaza")
+//                .append("peso",2000.00)
+//                .append("sexo","FEMININO")
+//                .append("level",8000)
+//                .append("dificuldade","DIFICIL")
+//                .append("tipos",Arrays.asList(
+//                        new Document("tipo1","DRAGON"),
+//                        new Document("tipo2","FLYING")
+//                        )
+//                )
+//                .append("raridade","SUPER_RARO");
+//
+//        pokemons.insertOne(novoPokemon);
+//
+//        Document novaMochila = new Document("_id",3)
+//                .append("quantidadePokeball",99)
+//                .append("quantidadeGreatball",99)
+//                .append("quantidadeMasterball",99)
+//                .append("quantidadeHeavyball",99)
+//                .append("quantidadeNetball",99);
+//        Document novaMochila1 = new Document("_id",4)
+//                .append("quantidadePokeball",0)
+//                .append("quantidadeGreatball",0)
+//                .append("quantidadeMasterball",0)
+//                .append("quantidadeHeavyball",0)
+//                .append("quantidadeNetball",0);
+//
+//        mochilas.insertMany(Arrays.asList(novaMochila,novaMochila1));
 
         //todo ATUALIZANDO CAMPO DO DOCUMENTO
-        pokemons.updateOne(Filters.eq("racaPokemon","Rayquaza"), new Document("$set", new Document("peso", 1000)));
+//        pokemons.updateOne(Filters.eq("racaPokemon","Rayquaza"), new Document("$set", new Document("peso", 1000)));
 
-        mochilas.updateOne(Filters.and(Filters.eq("_id",4), Filters.gt("quantidadePokeball",0)), new Document("$inc", new Document("quantidadePokeball",-1)));
-
+//        mochilas.updateOne(Filters.and(Filters.eq("_id",3), Filters.gt("quantidadePokeball",0)), new Document("$inc", new Document("quantidadePokeball",-1)));
         //todo DELETANDO DOCUMENTO
-//        pokemons.deleteOne(Filters.eq("racaPokemon" , "Rayquaza"));
+        pokemons.deleteOne(Filters.eq("racaPokemon" , "Rayquaza"));
 
-//        mochilas.deleteMany(Filters.gte("_id", 3));
+        mochilas.deleteMany(Filters.gte("_id", 3));
 
         //todo BUSCA DE UM DOCUMENTO
         System.out.println("Pokemon buscado:");
